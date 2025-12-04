1d0c9d3e2a41c92ad9c0b7b45f8f63c49a0da7d9
Milo Neumann <milo.rob.nl@gmail.com>
2025-12-04 11:55:21 -0800

    fixed design doc

--- design-doc.h
+++ design-doc.h
@@ -1,6 +1,13 @@
-// Shortest Path BFS-tree Final Design Doc
+// Shortest Path BFS-tree Final Project Design
 // Milo Neumann
 // Date: 12/2/25
+
+
+/*
+ABSTRACT
+input: set of nodes, set of edges, a source node, a pair of nodes
+output: the shortest path between the given pair of nodes
+*/
 
 /*
 DATA STRUCTURES
